ecd0f2b2e506dfc553e9ce8cea6d47209e89773b
ibrakunushefci <ibrakunushefci@gmail.com>
2020-08-21 21:37:37 +0200

    Added responsive mode

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // Check window width
+      width: window.innerWidth,
+
       items: BoardNumbers, // all board items
       currentItem: {
         count: ''
-      }
+      },
     }
+
+    this.updateDimensions = this.updateDimensions.bind(this);
   }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions); 
+  }
+
 
   // this function is used to update count and for setting current clicked item
   updateItem = (item) => {
@@ -36,10 +46,23 @@ class App extends React.Component {
     })
   }
 
+
   resetBoard() {
     this.setState({
       items: BoardNumbers
     })
+  }
+
+  // update device width so we can detect if we're in mobile and warn user to rotate its device into landscape mode
+  updateDimensions() {
+    this.setState({
+      height: window.innerHeight, 
+      width: window.innerWidth
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
   }
 
   render() {
@@ -54,60 +77,62 @@ class App extends React.Component {
 
     return (
       <>
-        <div onClick={() => this.resetBoard()}>Reset</div>
+        {this.state.width < 600 
+          ? <p style={{textAlign: "center"}}>Please put you device in landscape mode in order to play this game</p>
+          : (<div className="mainWrapper">
+              <div className="container-first">
+              <div className="zero-item">
+                <div className="value">0</div>
+              </div>
 
-        <div className="mainWrapper">
-            <div className="container-first">
-            <div className="zero-item">
-              <div className="value">0</div>
+              {boardItems}
+
+              <div className="column-item">
+                <div className="value">2-1</div>
+              </div>
+              <div className="column-item">
+                <div className="value">2-1</div>
+              </div>
+              <div className="column-item">
+                <div className="value">2-1</div>
+              </div>
             </div>
 
-            {boardItems}
-
-            <div className="column-item">
-              <div className="value">2-1</div>
+            <div className="container-second">
+              <div className="doz-item">
+                <div>1st 12</div>
+              </div>
+              <div className="doz-item">
+                <div>2nd 12</div>
+              </div>
+              <div className="doz-item">
+                <div>3rd 12</div>
+              </div>
             </div>
-            <div className="column-item">
-              <div className="value">2-1</div>
+            
+            <div className="container-third">
+              <div className="outside-section">
+                <div>1-18</div>
+              </div>
+              <div className="outside-section">
+                <div>EVEN</div>
+              </div>
+              <div className="outside-section">
+                <div><div className="rhomb-red"></div></div>
+              </div>
+              <div className="outside-section">
+                <div><div className="rhomb-black"></div></div>
+              </div>
+              <div className="outside-section">
+                <div>ODD</div>
+              </div>
+              <div className="outside-section">
+                <div>19-36</div>
+              </div>
+              <button className="reset-game" onClick={() => this.resetBoard()}>Reset Board</button>
             </div>
-            <div className="column-item">
-              <div className="value">2-1</div>
-            </div>
-          </div>
-
-          <div className="container-second">
-            <div className="doz-item">
-              <div>1st 12</div>
-            </div>
-            <div className="doz-item">
-              <div>2nd 12</div>
-            </div>
-            <div className="doz-item">
-              <div>3rd 12</div>
-            </div>
-          </div>
-          
-          <div className="container-third">
-            <div className="outside-section">
-              <div>1-18</div>
-            </div>
-            <div className="outside-section">
-              <div>EVEN</div>
-            </div>
-            <div className="outside-section">
-              <div><div className="rhomb-red"></div></div>
-            </div>
-            <div className="outside-section">
-              <div><div className="rhomb-black"></div></div>
-            </div>
-            <div className="outside-section">
-              <div>ODD</div>
-            </div>
-            <div className="outside-section">
-              <div>19-36</div>
-            </div>
-          </div>
-        </div>
+          </div>)
+        }
       </>
     );
   }
